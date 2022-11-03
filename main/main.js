b33915d9059da0b32b21c040011c85d7f6c5c1d7
aerbe12 <arya.wirya12@gmail.com>
2022-11-03 22:14:47 +0800

    pertama

---
+++ main/main.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  $.ajax({
+    type: "GET",
+  url: "../api/stats.json",
+  success: function(province){
+
+    var dataProvince = province.regions;
+    for (var list = 0; list <= dataProvince.length; list++) {
+      $("#dataProvince").append(
+        "<option value = " +
+        dataProvince[list].name +
+        ">" +
+        dataProvince[list].name +
+        "</option>"
+      );
+      
+    }
+  },
+  });
+
+
+$.ajax ({
+  type: "GET",
+  url: "../api/stats.json",
+  success: function(data){
+
+    $('#infected').append(data.numbers.infected+' case')
+    $('#recovered').append(data.numbers.recovered+' case')
+    $('#fatal').append(data.numbers.fatal+' case')
+
+    //  consloe log
+   console.log(data)
+  }
+
+})
+
+}
+)
